b235fad9442cae88e2e024afcfe211b59a0ffdfd
semir <semirjazvin3@gmail.com>
2025-12-07 11:47:27 +0100

    Set up fundation and added folders for images icones and fonts

---
+++ stella-bjelasnica/src/app/pages/home/home.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+
+export class Home {
+
+}
