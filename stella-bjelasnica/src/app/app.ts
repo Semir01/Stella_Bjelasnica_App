63b0e2f80527afb9521eff7ceb5e2d7f4d8e29cb
semir <semirjazvin3@gmail.com>
2025-12-08 20:33:36 +0100

    Fix some bugs

--- stella-bjelasnica/src/app/app.ts
+++ stella-bjelasnica/src/app/app.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
 import { Navbar } from './pages/navbar/navbar';
 import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +10,18 @@ import { CommonModule } from '@angular/common';
   imports: [RouterOutlet, Navbar, CommonModule],
   template: `
     <app-navbar *ngIf="showNavBar"></app-navbar>
-    <router-outlet (activate)="onActivate()"></router-outlet>
+    <router-outlet></router-outlet>
   `
 })
 export class App {
   showNavBar = true;
 
-  onActivate() {
-    // sakrij navbar samo na root/Home
-    this.showNavBar = window.location.pathname !== '/';
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+
+        this.showNavBar = event.urlAfterRedirects !== '/home';
+      });
   }
 }
