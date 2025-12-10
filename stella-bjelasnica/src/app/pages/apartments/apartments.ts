01eb86fa2dd949b0c314630efb32785a576240e3
semir <semirjazvin3@gmail.com>
2025-12-10 18:34:27 +0100

    Set slider for folurs and garages and set up indikators and titles

--- stella-bjelasnica/src/app/pages/apartments/apartments.ts
+++ stella-bjelasnica/src/app/pages/apartments/apartments.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-apartments',
-  imports: [],
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './apartments.html',
   styleUrl: './apartments.scss',
 })
 export class Apartments {
 
+
+  names = [
+    'Garaža 1',
+    'Garaža 2',
+    'Sprat 1',
+    'Sprat 2',
+    'Sprat 3',
+    'Sprat 4',
+    'Sprat 5'
+  ];
+
+   images = [
+    'assets/images/garaže1.jpg',
+    'assets/images/garaže2.jpg',
+    'assets/images/sprat1.jpg',
+    'assets/images/sprat2.jpg',
+    'assets/images/sprat3.jpg',
+    'assets/images/sprat4.jpg',
+    'assets/images/sprat5.jpg'
+  ];
+
+  currentIndex = 0;
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevSlide() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
 }
